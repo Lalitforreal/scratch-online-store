38d1daf1755d5de1d661a2f69764be03050b262b
Lalitforreal <s24cseu0093@bennett.edu.in>
2025-11-22 21:59:03 +0530

    Added the address side of things

---
+++ .history/models/user-model_20251122183654.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+     fullname : {
+        type : String,
+        minLength : 3,
+        trim : true
+     },
+     email : String,
+     password : String,
+      cart: [
+      {
+         product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'product'
+         },
+         quantity: {
+            type: Number,
+            default: 1
+         }
+      }],
+     orders : {
+        type : Array,
+        default : []
+     },
+     address: {
+         fullName: String,
+         phone: String,
+         pincode: String,
+         line1: String,
+         line2: String,
+         city: String,
+         state: String
+      }, 
+     contact : Number,
+     picture : String,
+     resetPasswordToken: String,
+      resetPasswordExpires: Date
+})
+
+module.exports = mongoose.model("user", userSchema);
---
+++ .history/models/user-model_20251122193605.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+     fullname : {
+        type : String,
+        minLength : 3,
+        trim : true
+     },
+     email : String,
+     password : String,
+      cart: [
+      {
+         product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'product'
+         },
+         quantity: {
+            type: Number,
+            default: 1
+         }
+      }],
+     orders : {
+        type : Array,
+        default : []
+     },
+     address: [{
+         fullName: String,
+         phone: Number,
+         pincode: Number,
+         line1: String,
+         line2: String,
+         city: String,
+         state: String,
+         isDefault: { type: Boolean, default: false }
+
+      }], 
+     contact : Number,
+     picture : String,
+     resetPasswordToken: String,
+      resetPasswordExpires: Date
+})
+
+module.exports = mongoose.model("user", userSchema);
